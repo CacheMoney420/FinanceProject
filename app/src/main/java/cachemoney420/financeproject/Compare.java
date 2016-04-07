59adf36b51695dddfe15a0ddf362a93b699b2464
Michael Spear <mwspear@gmail.com>
2016-04-07 12:08:58 -0500

    Created compare class

--- app/src/main/java/cachemoney420/financeproject/Compare.java
+++ app/src/main/java/cachemoney420/financeproject/Compare.java
@@ -1,25 +1,50 @@
 package cachemoney420.financeproject;
 
+import android.util.Log;
+
+import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
 
 
 /**
  * Created by mspear on 4/5/16.
  */
 public class Compare {
+    private static final String TAG = "tag";
 
+    public HashMap<BigDecimal, String> getCompare(String[] over, String[] under) {
 
-    public static void main(String[] args) {
-        ArrayList<String> tickerArray = new ArrayList<String>();
-        tickerArray.add("PFE");
-        tickerArray.add("BAC");
-        tickerArray.add("VRX");
+        HashMap<BigDecimal, String> ratioMap = new HashMap<BigDecimal, String>();
 
-        Calendar from = Calendar.getInstance();
-        Calendar to = Calendar.getInstance();
+        try {
+            Map<String, Stock> overMap = YahooFinance.get(over);
+            Map<String, Stock> underMap = YahooFinance.get(under);
+            for (String lv : overMap.keySet()) {
+                for (String ticker : underMap.keySet()) {
+                    BigDecimal ratio = overMap.get(lv).getQuote().getPrice().divide(
+                            underMap.get(ticker).getQuote().getPrice(), BigDecimal.ROUND_CEILING);
+                    ratioMap.put(ratio, lv + " vs " + ticker);
+                }
+            }
 
-        from.add(Calendar.YEAR, -1);
+        } catch (IOException ioe) {
+            Log.d(TAG, ioe.getMessage());
+        }
+        return ratioMap;
+
     }
-
+    public List<BigDecimal> sortCompares(HashMap<BigDecimal, String> ratioMap) {
+        List<BigDecimal> ratioList = new ArrayList<>(ratioMap.keySet());
+        Collections.sort(ratioList);
+        Collections.reverse(ratioList);
+        return ratioList;
+    }
 }
