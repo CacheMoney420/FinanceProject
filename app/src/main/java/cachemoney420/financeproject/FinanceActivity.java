c6337f5220cc3af8f2f20f16f290109081f2ac97
tommy-cool <trebers@gmail.com>
2016-04-14 12:06:49 -0500

    Big big stuff

--- app/src/main/java/cachemoney420/financeproject/FinanceActivity.java
+++ app/src/main/java/cachemoney420/financeproject/FinanceActivity.java
@@ -24,7 +24,7 @@ public class FinanceActivity extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_finance_list, container, false);
+        View view = inflater.inflate(R.layout.activity_finance, container, false);
 
         mComparisonRecyclerView = (RecyclerView) view.findViewById(R.id.comparison_recycler_view);
         mComparisonRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
