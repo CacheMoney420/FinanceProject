227a0a30340603ea8aaadc2f8ea266721ba41298
Michael Spear <mwspear@gmail.com>
2016-05-03 12:20:10 -0500

    Added Ratio and rank to the Comparison Fragment

--- app/src/main/java/cachemoney420/financeproject/ComparisonFragment.java
+++ app/src/main/java/cachemoney420/financeproject/ComparisonFragment.java
@@ -1,18 +1,14 @@
 package cachemoney420.financeproject;
 
 import android.database.sqlite.SQLiteDatabase;
-import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.helper.StaticLabelsFormatter;
@@ -23,10 +19,10 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
@@ -37,13 +33,13 @@ import yahoofinance.histquotes.Interval;
 public class ComparisonFragment extends Fragment {
 
     private static final String ARG_FINANCE_ID = "finance_id";
-    public static final String EXTRA_FINACE_ID = "extra_finance_id";
     private GraphView mGraphView;
 
-    private SQLiteDatabase mDatabase;
     private Comparison mComparison;
 
     private List<Double> mRatioList;
+    private TextView mRankDisplay;
+    private TextView mRatioDisplay;
 
     public static ComparisonFragment newInstance(UUID financeId) {
         Bundle args = new Bundle();
@@ -65,34 +61,44 @@ public class ComparisonFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_comparison, container, false);
         mGraphView = (GraphView) v.findViewById(R.id.graph);
+        new RatioTask().execute();
 
-        try {
-            mRatioList = new RatioTask().execute().get();
-        } catch (ExecutionException | InterruptedException e) {
-            mRatioList = new ArrayList<>();
-        }
-        System.out.println(mRatioList);
-        DataPoint[] data = new DataPoint[mRatioList.size()];
-        double x = 0;
-        for (int index = 0; index < mRatioList.size(); index++) {
-            DataPoint d = new DataPoint(x, mRatioList.get(index));
-            data[index] = d;
-            x++;
-        }
-        LineGraphSeries<DataPoint> lineGraphSeries = new LineGraphSeries<>(data);
-        mGraphView.addSeries(lineGraphSeries);
-        mGraphView.setTitle(mComparison.getOverweight() + " vs " + mComparison.getUnderweight());
-        mGraphView.getViewport().setXAxisBoundsManual(true);
-        mGraphView.getViewport().setMaxX(255);
-        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(mGraphView);
-        Calendar cal = Calendar.getInstance();
-        Date today = cal.getTime();
-        cal.add(Calendar.YEAR, -1);
-        DateFormat newDate = new SimpleDateFormat("MM/dd/yyyy");
-        staticLabelsFormatter.setHorizontalLabels(
-                new String[] {newDate.format(cal.getTime()), newDate.format(today)}
-        );
-        mGraphView.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+//        mGraphView = (GraphView) v.findViewById(R.id.graph);
+//
+//        try {
+//            mRatioList = new RatioTask().execute().get();
+//        } catch (ExecutionException | InterruptedException e) {
+//            mRatioList = new ArrayList<>();
+//        }
+//        System.out.println(mRatioList);
+//        DataPoint[] data = new DataPoint[mRatioList.size()];
+//        double x = 0;
+//        for (int index = 0; index < mRatioList.size(); index++) {
+//            DataPoint d = new DataPoint(x, mRatioList.get(index));
+//            data[index] = d;
+//            x++;
+//        }
+//        LineGraphSeries<DataPoint> lineGraphSeries = new LineGraphSeries<>(data);
+//        lineGraphSeries.setDrawBackground(true);
+//        mGraphView.addSeries(lineGraphSeries);
+//        mGraphView.setTitle(mComparison.getOverweight() + " vs " + mComparison.getUnderweight());
+//        mGraphView.getViewport().setXAxisBoundsManual(true);
+//        mGraphView.getViewport().setMaxX(255);
+//        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(mGraphView);
+//        Calendar cal = Calendar.getInstance();
+//        Date today = cal.getTime();
+//        cal.add(Calendar.YEAR, -1);
+//        DateFormat newDate = new SimpleDateFormat("MM/dd/yyyy", Locale.getDefault());
+//        staticLabelsFormatter.setHorizontalLabels(
+//                new String[] {newDate.format(cal.getTime()), newDate.format(today)}
+//        );
+//        mGraphView.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
+        mRankDisplay = (TextView) v.findViewById(R.id.displayRank);
+        mRankDisplay.setText(mComparison.getRank().toString());
+        mRatioDisplay = (TextView) v.findViewById(R.id.displayRatio);
+        mRatioDisplay.setText(mComparison.getRatio().toString());
+
         return v;
     }
 
@@ -107,6 +113,34 @@ public class ComparisonFragment extends Fragment {
                 this.cancel(true);
             }
             return new ArrayList<>();
+        }
+
+        @Override
+        protected void onPostExecute(List<Double> result) {
+            mRatioList = result;
+            System.out.println(mRatioList);
+            DataPoint[] data = new DataPoint[mRatioList.size()];
+            double x = 0;
+            for (int index = 0; index < mRatioList.size(); index++) {
+                DataPoint d = new DataPoint(x, mRatioList.get(index));
+                data[index] = d;
+                x++;
+            }
+            LineGraphSeries<DataPoint> lineGraphSeries = new LineGraphSeries<>(data);
+            lineGraphSeries.setDrawBackground(true);
+            mGraphView.addSeries(lineGraphSeries);
+            mGraphView.setTitle(mComparison.getOverweight() + " vs " + mComparison.getUnderweight());
+            mGraphView.getViewport().setXAxisBoundsManual(true);
+            mGraphView.getViewport().setMaxX(255);
+            StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(mGraphView);
+            Calendar cal = Calendar.getInstance();
+            Date today = cal.getTime();
+            cal.add(Calendar.YEAR, -1);
+            DateFormat newDate = new SimpleDateFormat("MM/dd/yyyy", Locale.getDefault());
+            staticLabelsFormatter.setHorizontalLabels(
+                    new String[] {newDate.format(cal.getTime()), newDate.format(today)}
+            );
+            mGraphView.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
         }
     }
 }
